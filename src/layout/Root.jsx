60f4382845fceaec0d5a00465e5f1511399ce285
AsaduzzamanSohun <asaduzzamansohun5@gmail.com>
2024-05-12 22:33:03 +0600

    footer section done

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const Root = () => {
     return (
@@ -8,6 +9,7 @@ const Root = () => {
             <div className="font-lato">
                 <Outlet></Outlet>
             </div>
+            <Footer></Footer>
         </div>
     );
 };
